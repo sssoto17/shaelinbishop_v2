2f2eefcbf00824115c7929655af6a37fb1bbc9d8
Soto <saso0001@stud.ek.dk>
2026-06-06 18:14:34 +0200

    update deploy

--- src/lib/loader.js
+++ src/lib/loader.js
@@ -3,8 +3,8 @@
 const payloadUrl = process.env.NEXT_PUBLIC_PAYLOAD_URL;
 
 export default function imageLoader({ src, width, quality }) {
-    const source = new URL(src, payloadUrl || 'http://localhost:3000');
-    const payloadOrigin = payloadUrl ? new URL(payloadUrl).origin : source.origin
+    const fallbackBase = payloadUrl || 'http://localhost:3000'
+    const source = new URL(src, fallbackBase);
 
     let pathname = source.pathname
     
@@ -15,18 +15,21 @@ export default function imageLoader({ src, width, quality }) {
         // directPath = `media${path[1]}`;
     }
 
+    // if (pathname.startsWith('/api/images/file')) {
+    //     pathname = pathname.replace('/api/images/file', '/images/file')
+    // }
+
     const isLocalHost =
     source.hostname === 'localhost' ||
     source.hostname === '127.0.0.1' ||
     source.hostname === '::1'
     
     const origin = isLocalHost && payloadUrl ? new URL(payloadUrl).origin : source.origin
-    const test = `${origin}${pathname}?w=${width}&q=${quality || 75}`
-    // const test = `${url.protocol}//${url.host}${directPath}?w=${width}&q=${quality || 75}`;
+    return`${origin}${pathname}?w=${width}&q=${quality || 75}`
 
     // console.log("HOST:", url.host)
     // console.log("HOST:", process.env.NEXT_PUBLIC_PAYLOAD_URL)
-    console.log("IMG:", test)
+    // console.log("IMG:", test)
 
     return test
 }
